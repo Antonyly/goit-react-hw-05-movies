2a0bc77312051b0d4e368fcb4c1fc12023b52ed7
Antonyly <antonlyzun@gmail.com>
2022-11-02 17:48:38 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,9 @@ const HeaderMenu = lazy(() => import("../modules/HeaderMenu"));
 const HomePage = lazy(() => import("../pages/HomePage"));
 const SearchPage = lazy(() => import("../pages/SearchPage"));
 const SingleMoviePage = lazy(() => import("../pages/SingleMoviePage"));
+const CastPage = lazy(() => import("../pages/CastPage"));
+const ReviewsPage = lazy(() => import("../pages/ReviewsPage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
 export const App = () => {
     return (
@@ -14,12 +17,12 @@ export const App = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/movies" element={<SearchPage />} />
-        <Route path="movies/:id" element={<SingleMoviePage />}>
-          {/* <Route path="cast" element={<CastPage />} /> */}
-          {/* <Route path="reviews" element={<ReviewsPage />} /> */}
-        </Route> */
-        {/* /* <Route path={"*"} element={<NotFoundPage />} /> */}
+                <Route path="movies/:id" element={<SingleMoviePage />}>
+                    <Route path="cast" element={<CastPage />} />
+                    <Route path="reviews" element={<ReviewsPage />} />
+                </Route>
+                <Route path={"*"} element={<NotFoundPage />} />
             </Routes>
         </Suspense>
     );
-}
+};
