4c81e5cf8279bd9c89802522067272f4434aa767
Antonyly <antonlyzun@gmail.com>
2022-11-04 17:56:32 +0200

    +

--- src/components/MoviesItem/MoviesItem.jsx
+++ src/components/MoviesItem/MoviesItem.jsx
@@ -1,6 +1,7 @@
 import { nanoid } from "nanoid";
 import PropTypes from "prop-types";
 import s from "./moviesItem.module.css"
+import { Link, useLocation } from "react-router-dom";
 
 const MoviesItem = ({ item }) => {
     const {
@@ -14,34 +15,54 @@ const MoviesItem = ({ item }) => {
     const genresItem = genres.map(item => (
         <li key={nanoid()}>{item.name}</li>
     ));
+    const getActiveLink = ({ isActive }) => {
+        return isActive ? s.itemActive : s.item;
+    };
     const releaseDate = release_date.slice(0, 4)
     const voteAverage = (vote_average * 10).toFixed(0);
+    const location = useLocation();
+    const comeBack = location.state?.from || '/'
     
     return (
-        <div className={s.container}>
-            <img
-                className={s.img}
-                src={`https://image.tmdb.org/t/p/original${poster_path}`}
-                alt={title}
-                width="300"
-                height="400"
-            />
-            <div >
-                <h2>{`${title} (${releaseDate})`}</h2>
-            <p>
-              USER SCORE: <span className={s.text}>{voteAverage}%</span>
-            </p>
-            <p>
-              OVERVIEW: <span className={s.text}>{overview}</span>
-            </p>
-            <p>GENRES:</p>
-            <ul className={s.list}>
-                {genresItem}
-            </ul>
+        <>
+            <Link to={comeBack}>Go back</Link>
+            <div className={s.container}>
+                <img
+                    className={s.img}
+                    src={`https://image.tmdb.org/t/p/original${poster_path}`}
+                    alt={title}
+                    width="300"
+                    height="400"
+                />
+                <div >
+                    <h2>{`${title} (${releaseDate})`}</h2>
+                    <p>
+                        USER SCORE: <span className={s.text}>{voteAverage}%</span>
+                    </p>
+                    <p>
+                        OVERVIEW: <span className={s.text}>{overview}</span>
+                    </p>
+                    <p>GENRES:</p>
+                    <ul className={s.list}>
+                        {genresItem}
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
-}
+            <ul className={s.secList}>
+                <li className={s.secListIt}>
+                    <Link className={getActiveLink} to='cast' state={{ from: comeBack }}>
+                        Cast
+                    </Link>
+                </li>
+                <li className={s.secListIt}>
+                    <Link className={getActiveLink} to='reviews' state={{ from: comeBack }}>
+                        Reviews
+                    </Link>
+                </li>
+            </ul>
+        </>
+    );
+};
 
 MoviesItem.defaultProps = {
   item: [],
--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, NavLink, Outlet, Link, useLocation } from "react-router-dom";
+import { useParams, Outlet } from "react-router-dom";
 import MoviesItem from '../../components/MoviesItem';
 
 import { getMovieById } from '../../shared/api/services';
@@ -19,9 +19,9 @@ const SingleMoviePage = () => {
     const { id: StrId } = useParams();
   const id = Number(StrId);
 
-    const location = useLocation();
+    // const location = useLocation();
 
-     const goBack = location?.state?.from ?? '/';
+    //  const goBack = location.state?.from || '/';
 
     useEffect(() => {
         const fetchMovies = async () => {
@@ -50,24 +50,24 @@ const SingleMoviePage = () => {
         
     }, [id]);
 
-    const getActiveLink = ({isActive}) => {
-    return isActive ? s.itemActive : s.item;
-}
+//     const getActiveLink = ({isActive}) => {
+//     return isActive ? s.itemActive : s.item;
+// }
 
     
     const isMovie = Object.keys(movie).length > 0;
     return (
         <>
             <div className={s.container}>
-                <Link className={s.button} to={goBack}>
+                {/* <Link className={s.button} to={goBack}>
                     Go back
-                </Link>
+                </Link> */}
             
                 {loading && <p>...Loading</p>}
                 {error && <p>Movie not found</p>}
                 {isMovie && <MoviesItem item={movie} />}
-                {isMovie && <NavLink className={getActiveLink} to={`cast`} state={{ from: goBack }}>Cast</NavLink>}
-                {isMovie && <NavLink className={getActiveLink} to={`reviews`} state={{ from: goBack }}>Reviews</NavLink>}
+                {/* {isMovie && <NavLink className={getActiveLink} to={`cast`} state={{ from: goBack }}>Cast</NavLink>}
+                {isMovie && <NavLink className={getActiveLink} to={`reviews`} state={{ from: goBack }}>Reviews</NavLink>} */}
                 
             </div>
             <Outlet />
